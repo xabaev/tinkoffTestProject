29a8cca14d444c1d64fc8b188bb990e3392cd1b0
xabaev <lighttraf93@gmail.com>
2018-05-03 17:57:56 +0300

    Описание страницы Оплатить ЖКУ в Москве

--- src/test/java/SingleTest.java
+++ src/test/java/SingleTest.java
@@ -5,9 +5,7 @@ import pages.bank.payments.categories.communal.zhkuMoskva.PayZhkuMoskva;
 import pages.topPanel.SecondMenu;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
 
 public class SingleTest {
 
@@ -19,16 +17,16 @@ public class SingleTest {
 
     @Test
     public void Test1() {
-        SecondMenu currentPage = new SecondMenu() //после открытия страницы находимся на главной. Главная не описана, так что SecondTab
+        CommunalPaymentPage navigatePage = new SecondMenu() //после открытия страницы находимся на главной. Главная не описана, так что SecondTab
                 .tabPayments()
                 .clickCommunalPayment();
-        if (((CommunalPaymentPage) currentPage).getRegionPaymentText() != "г. Москва") { //если регион не "г. Москва", то откроем такой. ЗЫ: задание написано криво, т.к. "г. Москва" не может быть тут.
-            ((CommunalPaymentPage) currentPage).clickRegionPayment()
+        if (navigatePage.getRegionPaymentText() != "г. Москва") { //если регион не "г. Москва", то откроем такой. ЗЫ: задание написано криво, т.к. "г. Москва" не может быть тут.
+            navigatePage.clickRegionPayment()
                     .selectRegionFromTable("г. Москва");
         }
-        String savePayment = ((CommunalPaymentPage) currentPage).getTextCustomElement(1); //Сохраним, что у нас первым в списке оплат
+        String savePayment = navigatePage.getTextCustomElement(1); //Сохраним, что у нас первым в списке оплат
         assertEquals(savePayment, "ЖКУ-Москва");
-        ((CommunalPaymentPage) currentPage).clickZhkuMoskva()//Продолжаем открывать странички
+        Object currentPage = navigatePage.clickZhkuMoskva()//Продолжаем открывать странички
                 .clickPayZhku()
                 .clickPay();
     }
